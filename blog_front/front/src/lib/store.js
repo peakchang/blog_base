91754a4cfc96e6b03dcc8dd0a7b90914f3ccd594
pcy <ridebus82@nate.com>
2023-12-20 09:16:17 +0900

    231220 gogofofigogogogo

--- blog_front/front/src/lib/store.js
+++ blog_front/front/src/lib/store.js
@@ -7,3 +7,5 @@ export let authStatus = writable('');
 export let testCookie = writable('');
 
 export let seoValue = writable({});
+
+export let testStoreArr = writable([1,2,3,4,5,6,7,8,9,11]);
--- blog_front/front/src/routes/adm/subdomain/writeable/+page.svelte
+++ blog_front/front/src/routes/adm/subdomain/writeable/+page.svelte
@@ -1,9 +1,20 @@
 <script>
     import Sortable from "sortablejs";
     import { onMount } from "svelte";
-    import { admin_sidebar } from "$src/lib/store";
+    import { admin_sidebar, testStoreArr } from "$src/lib/store";
 
     let sortableEle;
+    let testArr = $testStoreArr;
+    let number;
+
+    // 이미지 추가 등 할때 처리 하기 (추가로 만져보자)
+    function goaction() {
+        const tempArr = [...testArr]
+        tempArr.push(Number(number))
+        testArr = [...tempArr]
+        $testStoreArr = testArr;
+
+    }
 
     onMount(() => {
         new Sortable(sortableEle, {
@@ -12,33 +23,50 @@
             //요소 클릭(이동)중, ghostClass에 할당 된 값을 해당 요소의 Class명으로 부여
             ghostClass: "active",
             onEnd: function (evt) {
-                var itemEl = evt.item; // dragged HTMLElement
-                evt.to; // target list
-                evt.from; // previous list
-                evt.oldIndex; // element's old index within old parent
-                evt.newIndex; // element's new index within new parent
-                evt.oldDraggableIndex; // element's old index within old parent, only counting draggable elements
-                evt.newDraggableIndex; // element's new index within new parent, only counting draggable elements
-                evt.clone; // the clone element
-                evt.pullMode; // when item is in another sortable: `"clone"` if cloning, `true` if moving
-            },
-            onChange: function (/**Event*/ evt) {
-                evt.newIndex; // most likely why this event is used is to get the dragging element's current index
-                // same properties as onEnd
-                console.log(evt.newIndex);
+                $testStoreArr = arrChangeFunc(
+                    $testStoreArr,
+                    evt.oldIndex,
+                    evt.newIndex,
+                );
+                console.log($testStoreArr);
             },
         });
     });
+
+    function arrChangeFunc(getArr, oldIndex, newIndex) {
+        const value = getArr[oldIndex];
+        getArr.splice(oldIndex, 1);
+        getArr.splice(newIndex, 0, value);
+        return getArr;
+    }
 </script>
 
-<div class="mt-14 px-5 text-sm suit-font" class:ml-52={!$admin_sidebar}>
-    <div>
+<div class="mt-14 px-5 suit-font" class:ml-52={!$admin_sidebar}>
+    <div class="text-sm">
         <h1>drag and drop</h1>
         <ul class="cursor-pointer flex gap-1" bind:this={sortableEle}>
-            <li class="w-9 h-9 border">list1</li>
-            <li class="w-9 h-9 border">list2</li>
-            <li class="w-9 h-9 border">list3</li>
-            <li class="w-9 h-9 border">list4</li>
+            {#each testArr as chk}
+                <li class="w-14 h-14 border">list{chk}</li>
+            {/each}
         </ul>
+    </div>
+
+    <div>
+        <input type="text" bind:value={number}>
+        <button
+            class="bg-blue-700 text-white rounded-lg mt-5 py-1 px-3"
+            on:click={goaction}
+        >
+            변경하기!
+        </button>
+        
+        <button
+            class="bg-orange-700 text-white rounded-lg mt-5 py-1 px-3"
+            on:click={() => {
+                console.log(testArr);
+            }}
+        >
+            배열에 추가!
+        </button>
     </div>
 </div>
